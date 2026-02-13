103046d645e87232e76acb77556c38ce216aface
Caio Cezar <caiocezar.bezerra@hotmail.com>
2026-02-13 11:45:40 -0300

    first commit

---
+++ calculadora/src/components/Button/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 40px;
+    border: 1px solid #CDCDCD;
+    background-color: #00AAF0;
+    color: #FFFFFF;
+    font-size: 20px;
+    font-weight: 600;
+    flex: 1;
+
+
+    &:hover{
+        opacity: 0.6;
+    }
+`
+
+
